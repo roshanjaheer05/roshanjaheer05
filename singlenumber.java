34cb31e6fc5e430849142bc018c6e6cfea602748
roshanjaheer05 <140388184+roshanjaheer05@users.noreply.github.com>
2026-02-19 21:27:24 +0530

    Add files via upload

---
+++ singlenumber.java
@@ -0,0 +1,9 @@
+public class singlenumber {
+    public int singleNumber(int[] nums){
+        int result=0;
+        for(int num:nums){
+            result ^=num;
+        }
+        return result;
+    }
+}

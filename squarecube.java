1c5533f784da48dca35abf133610033f049c8d82
roshanjaheer05 <140388184+roshanjaheer05@users.noreply.github.com>
2026-02-25 11:08:22 +0530

    Add files via upload

---
+++ squarecube.java
@@ -0,0 +1,10 @@
+public class squarecube{
+    public static void main(String[] args){
+        Scanner sc=new SCanner(System.in);
+        int num=sc.nextInt();
+        int square=num*num;
+        int cube=num*num*num;
+        System.out.println("Square: "+square);
+        System.out.println("Cube: "+cube);
+    }
+}
